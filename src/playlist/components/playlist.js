2713c951078adfb4fc8e2e809cb2e8fe7f6bada6
Yamid V.M <yavemu@gmail.com>
2018-11-27 15:58:02 -0500

    Convirtiendo componente a Componente funcional

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,19 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import  Media from "./media";
 
-class Playlist extends Component{
-    render(){
-        const playlist = this.props.data.categories[0].playlist;
-        return (
-            <div>
-                {
-                    playlist.map((item) => {
-                        return <Media {...item} key={item.id} />
-                    })
-                }
-            </div>
-        )
-    }
+function Playlist(props) {
+    const playlist = props.data.categories[0].playlist;
+    return (
+        <div>
+            {
+                playlist.map((item) => {
+                    return <Media {...item} key={item.id} />
+                })
+            }
+        </div>
+    )
 }
 
 export default Playlist;
