4bd5147c73b5012f4ab031c8c739e35f919a613e
Yamid V.M <yavemu@gmail.com>
2019-01-16 19:42:38 -0500

    Componentes "Inteligentes" y "tontos"

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,10 +1,13 @@
 import React from "react";
-import  Media from "./media";
+import Media from "./media";
+import Play from "../../icons/components/play";
 
 function Playlist(props) {
+   
     const playlist = props.data.categories[0].playlist;
     return (
         <div>
+            <Play size={50} color="red"/>
             {
                 playlist.map((item) => {
                     return <Media {...item} key={item.id} />
