02af7b6a9fa7c48c41f93cbc23e3ec32d9a1eca5
Yamid V.M <yavemu@gmail.com>
2019-01-16 20:08:27 -0500

    Completando los iconos - usando componentes "tontos"

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Media from "./media";
 import Play from "../../icons/components/play";
+import Pause from "../../icons/components/pause";
+import Volume from "../../icons/components/volume";
+import Fullscreen from "../../icons/components/fullscreen";
 
 function Playlist(props) {
    
@@ -8,6 +11,10 @@ function Playlist(props) {
     return (
         <div>
             <Play size={50} color="red"/>
+            <Pause size={50} color="red"/>
+            <Volume size={50} color="red"/>
+            <Fullscreen size={50} color="red"/>
+            <br />
             {
                 playlist.map((item) => {
                     return <Media {...item} key={item.id} />
