e43e54dd6db4ff5ba0bc45239909edcf6429998d
Yamid V.M <yavemu@gmail.com>
2019-01-16 22:48:27 -0500

    Agregando nueva seccion de la pagina

---
+++ src/pages/components/related.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import logo from '../../../public/images/logo.png';
+
+function Related(props) {
+    return (
+        <div>
+            <h3>AQUI VA EL LOGO</h3>
+            {/* <img src={logo} width={250} /> */}
+        </div>
+    )
+}
+
+export default Related;
--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import HomeLayout from "../components/home-layout";
 import data from "../../api.json";
 import Categories from "../../categories/components/categories";
+import Related from "../components/related";
 
 class Home extends Component{
     render() {
         return (
             <HomeLayout>
+                <Related />
                 <Categories categories={data.categories}></Categories>
             </HomeLayout>
         )
