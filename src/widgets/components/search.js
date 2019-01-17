34315884b07cb387379a77e2e0168c6dcce523df
Yamid V.M <yavemu@gmail.com>
2019-01-17 12:22:18 -0500

    Manejador de eventos para el formulairo y el input de busqueda

--- src/widgets/components/search.js
+++ src/widgets/components/search.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import "./search.css";
 
-const Search = () => (
-  <form className="Search">
+const Search = (props) => (
+  <form 
+    className="Search"
+    onSubmit={props.handleSubmit}
+    >
     <input
+      ref={props.setRef}
       type="text"
       className="Search-input"
       placeholder="Busqueda filtrada"
+      name='search'
+      value={props.value}
+      onChange={props.handleChange}
     />
   </form>
 );
--- src/widgets/containers/search.js
+++ src/widgets/containers/search.js
@@ -1,11 +1,38 @@
 import React,{ Component } from "react";
 import Search from "../components/search";
 
-class SearchContainer extends Component{
-    render(){
-        return (
-            <Search></Search>
-        )
+class SearchContainer extends Component {
+
+    state = {
+        value: 'Valor por defecto'
+    }
+    
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.input.value,'Submit');
+    }
+    
+    handleInputChange = (e) => {
+        const value = e.target.value;
+
+        this.setState({
+            'value': value.replace(" ","-")
+        })
+
+        console.log('Change');
+    }
+
+    setRef = element => {
+        this.input = element
+    }
+
+    render() {
+        return <Search 
+            handleSubmit={this.handleSubmit}
+            handleChange={this.handleInputChange}
+            setRef={this.setRef}
+            value={this.state.value}
+        />;
     }
 }
 
